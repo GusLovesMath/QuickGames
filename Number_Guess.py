785e09570527df0c7827d18d615c3d15fab46ed6
GusLovesMath <109978367+GusLovesMath@users.noreply.github.com>
2022-07-26 23:03:33 -0400

    Add files via upload

---
+++ Number_Guess.py
@@ -0,0 +1,24 @@
+def Guess_Number( n=10 ):
+
+    import random as r
+
+    number = r.randint( 0 , n + 1)
+    T = ['first', 'second', 'third']
+
+    Guess = 0
+    while Guess <= 3:
+
+        if Guess == 3:
+            print('You lost the game, run again to try again :(')
+            break
+
+        elif int( input(f'What is your {T[Guess]} guess of range 0 to {n}: ') ) != number:
+            Guess +=1
+            print('Try again :(\n')
+
+        else:
+            print(f'You guessed the right number, {number}!')
+            break
+        
+
+#Guess_Number()
