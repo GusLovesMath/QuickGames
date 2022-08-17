084223f2e169d88d6d1d6b17e55dcfac31831e1c
GusLovesMath <gyacteen@gmail.com>
2022-08-17 10:33:46 -0400

    Update Dice_Roll.py

--- Dice_Roll.py
+++ Dice_Roll.py
@@ -1,4 +1,4 @@
-def dice_roll(Count=True, Rolls=False, Hist=False):
+def dice_roll(Count=True, Rolls=False, Stats=True, Hist=False):
     """
     A dice simulator the rolls m dice n times.
     Count=True returns the frequency each value appears
@@ -9,11 +9,13 @@ def dice_roll(Count=True, Rolls=False, Hist=False):
     import random as r
     from collections import Counter
     import matplotlib.pyplot as pl
+    import pandas as pd
 
     m = int( input('Number of dice you would like to roll: ') )
     n = int( input(f'Number of times you would like to roll {"{:,}".format(m)} dice: ') )
     
     print(f'You have choosen to roll {"{:,}".format(m)} dice {"{:,}".format(n)} times.')
+
 
     # Restricted amount of dice that can be rolled
     if m > 1200:
@@ -36,12 +38,20 @@ def dice_roll(Count=True, Rolls=False, Hist=False):
             if n > 750:
                 print('Will not show list of values for number of rolls greater than 750.')
 
+
         # Counts the frequency of values rolled
         if Count == True and m <= 40:
             orderd = Counter( rolls )
             print( sorted(orderd.items(), key=lambda i: i[0]) )   # orders rolls from smalles rolled to greatest with its frequency 
         else:
             print('Counter is unavailable for number of dice greater than 40.')
+
+
+        # printing statistics
+        if Stats == True:
+            print('Here are the general statistics:')
+            df = pd.DataFrame( rolls )
+            print( df.describe() ) 
 
 
         # Plots a histogram of values rolled
